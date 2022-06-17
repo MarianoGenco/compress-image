d852d70dd39c60ff2aa417544f6292267578b9b5
Mariano Genco <67137186+MarianoGenco@users.noreply.github.com>
2022-06-17 20:01:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+
+downloadsFolder = "C:/Users/..."
+
+if __name__ == "__main__":
+    for filename in os.listdir(downloadsFolder):
+        name, extension = os.path.splitext(downloadsFolder + filename)
+
+        try:
+            if extension in [".JPG", ".JPEG", ".PNG", ".jpg", ".jpeg", ".png"]:
+                picture = Image.open(downloadsFolder + filename)
+                picture.save(downloadsFolder + "comp_" + filename, optimize=True, quality=60)
+                picture = Image.open(downloadsFolder + filename)
+                picture.close()
+                os.remove(downloadsFolder + filename)
+                print(name + extension)
+        except:
+            continue            
+
